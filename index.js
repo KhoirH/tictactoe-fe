9e27b7fc2181711189b5e6c4cf33530224813e95
KhoirH <hilmi.khoirulloh02@gmail.com>
2022-03-23 21:08:30 +0700

    finish fe

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ let roomId = false;
 //
 let dataTictactoe = {};
 let user = {};
+let statusGame = false;
+let activePlayUser = false;
 
 if(getCookie('roomId') === '') {
   roomId = makeid(8);
@@ -20,14 +22,7 @@ if(getCookie('roomId') === '') {
 
 socket.on('playerAction', handlePlayerAction);
 
-function initGame() {
-  // user.symbol = 'x';
-  
-  // socket.emit('initGame', {
-  //   roomId : roomId,
-  //   user: user,
-  // });
-
+function startGameDom() {
   // setCookie('roomId', roomId);
   // setCookie('user', user);
 
@@ -37,27 +32,71 @@ function initGame() {
   const statiTitle = 'Code';
   const title = statiTitle + ' : ' + roomId;
   document.getElementById('title').innerText= title;
+}
+function initGame() {
+  // socket init game
+  user.symbol = 'x';
   
+  socket.emit('initGame', {
+    roomId : roomId,
+    user: user,
+  });
+  startGameDom();
+
+}
+
+function joinGame(event, component) {
+  event.preventDefault();
+  const inputCodeVal = document.getElementById('input-code').value;
+  roomId = inputCodeVal;
+  if(inputCodeVal !== '') {
+    user.symbol = 'o';
+    socket.emit('joinGame', {
+      roomId : inputCodeVal,
+      user: user,
+    }) 
+    startGameDom();
+  }
 }
 
 function boxClicked(component) {
-  // socket.emit('hitBox', {
-  //   user,
-  //   roomId,
-  //   id: component.id,
-  // })
 
-  // dom javascript
-  component.innerHTML = "<img alt='' src='/assets/x-solid.svg'/>";
-
+  if(statusGame === 'start' && activePlayUser === user.id) {
+    // socket hitbox
+    socket.emit('hitBox', {
+      user: user,
+      roomId: roomId,
+      boxId: parseInt(component.id.split('box-').join('')),
+    })
+  }
 }
 
 function handlePlayerAction(data) {
   dataTictactoe = data.dataTictactoe;
   dataArrayTictactoe = Object.entries(dataTictactoe);
+  statusGame = data.status;
+  activePlayUser = data.activeUser;
 
-  dataTictactoe.map(([id, symbol]) => {
-    document.getElementById(id).innerHTML = "<img alt='' src='/assets/"+symbol+".svg'/>";
+
+  if(data.status === 'end') {
+    if (data.winner === user.id) {
+      alert('You are winner');
+    } else {
+      alert('You are looser');
+    }
+    window.location.reload();
+  }
+  //dom status
+  document.getElementById('status-game').innerText= statusGame;
+  // change color
+  if(data.status === 'pending'){
+    document.getElementById('status-game').setAttribute('style', 'color:red')
+  } else {
+    document.getElementById('status-game').setAttribute('style', 'color:green')
+  }
+
+  dataArrayTictactoe.map(([id, object]) => {
+    document.getElementById('box-'+id).innerHTML = "<img alt='' src='/assets/"+object.symbol+"-solid.svg'/>";
   })
 }
 
